bdbb2da5f2ec22e6cfbb72b0b58af6a982dd3d9c
Nguyen Tung Duong <sieunhan283@gmail.com>
2024-06-10 10:33:28 +0700

    Optimizers outline

---
+++ optimizers.py
@@ -0,0 +1,21 @@
+class Optimizer:
+    """Base class for optimizers."""
+    def __init__(self):
+        pass
+    
+    def update(self, parameters, gradients):
+        """Updates the parameters based on gradients."""
+        pass
+
+class SGD(Optimizer):
+    """Stochastic Gradient Descent optimizer."""
+    def __init__(self, learning_rate):
+        """
+        Initializes the SGD optimizer.
+        :param learning_rate: Learning rate for updates.
+        """
+        pass
+    
+    def update(self, parameters, gradients):
+        """Updates parameters using SGD."""
+        pass
