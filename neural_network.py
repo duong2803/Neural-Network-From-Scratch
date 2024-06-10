eaa5e49ef123aea277e426b5db586318d0705218
Nguyen Tung Duong <sieunhan283@gmail.com>
2024-06-10 10:37:14 +0700

    Neural network outlines

---
+++ neural_network.py
@@ -0,0 +1,47 @@
+class NeuralNetwork:
+    """Class representing a neural network."""
+
+    def __init__(self):
+        """Initializes the neural network."""
+        pass
+
+    def add(self, layer):
+        """Adds a layer to the neural network."""
+        pass
+
+    def set_loss(self, loss_function):
+        """Sets the loss function for the neural network."""
+        pass
+
+    def set_optimizer(self, optimizer):
+        """Sets the optimizer for the neural network."""
+        pass
+
+    def forward(self, X):
+        """Performs the forward pass through the network."""
+        pass
+
+    def backward(self, y_true):
+        """Performs the backward pass through the network."""
+        pass
+
+    def update(self):
+        """Updates the network parameters."""
+        pass
+
+    def train(self, X, y, epochs, batch_size):
+        """
+        Trains the neural network.
+
+        Params
+        ------------------
+        X: Training data.
+        y: Training labels.
+        epochs: Number of epochs to train.
+        batch_size: Size of each training batch.
+        """
+        pass
+
+    def predict(self, X):
+        """Predicts outputs for the given input data."""
+        pass
